8e583883cca1f24b067cb1370f02af1b10531e5d
Abisola Adebiyi <abisola.ca@gmail.com>
2023-09-15 15:05:05 -0700

    update scripts

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ import cors from "cors"
 import 'dotenv/config'
 
 const app = express();
-app.use(cors())
 app.use(express.json());
+
+app.use(cors())
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
